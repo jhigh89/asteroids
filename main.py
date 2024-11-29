e11626a9159190014680d2a13f8f1f7a8990b3a4
jhigh89 <jhightower928@gmail.com>
2024-11-29 01:17:15 -0800

    forgot to get rid of the print statements

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
