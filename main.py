bdcf0d934eb2f7017f61181516c347dbb45ffb8f
jhigh89 <jhightower928@gmail.com>
2024-11-30 21:52:08 -0800

    added player sprite drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,11 +8,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # limits framerate to 60
 
