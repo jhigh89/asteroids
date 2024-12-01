d27a341b7b38f3ef058197c4bdaee69358ceee9f
jhigh89 <jhightower928@gmail.com>
2024-11-30 19:35:57 -0800

    added clock object to make use of FPS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # limits framerate to 60
 
 if __name__ == "__main__":
     main()
