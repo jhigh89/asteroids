30eb7750b083e258a17869bca2aff8dca26b9f3f
jhigh89 <jhightower928@gmail.com>
2024-12-01 00:24:15 -0800

    added pygame sprite groups

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for update in updatable:
+            update.update(dt)
         screen.fill(color="black")
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # limits framerate to 60
 
